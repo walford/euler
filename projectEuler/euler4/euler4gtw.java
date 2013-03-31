686c1174e59d3cf0c0eaf47ea533bf6b1b62d966
bpwalford <bpwalford@gmail.com>
2013-03-31 09:36:16 -0600

    adding euler4gtw edits (as git system test?)

--- projectEuler/euler4/euler4gtw.java
+++ projectEuler/euler4/euler4gtw.java
@@ -5,18 +5,26 @@ public class euler4gtw{
 public static void main(String[] argv){
 	int x = 0;
 
-	for(int i = 1; i<=99; i++){
+	for(int i = 1; i<10; i++){
 
-		for(int j = 1; j<=99; j++){
+		for(int j = 1; j<10; j++){
 			x = i*j;
-			// out.println(x);
+			out.println(x);
 
 			String pal = " " + x;
 			String lap = new StringBuilder(pal).reverse().toString();
 
-			if(pal.equals(lap)){
-				out.println(i +" ++ " + j + " == " + pal + " // " + lap);
+			out.print(pal + " ==> " + lap);
+
+			if(pal.equals(lap)) {
+				out.println("YAY!"); 
+			} else {
+				out.println("No");
 			}
+
+			// if(pal.equals(lap)){
+			// 	out.println(i +" ++ " + j + " == " + pal + " // " + lap);
+			// }
 
 
 		}// END FOR 2
