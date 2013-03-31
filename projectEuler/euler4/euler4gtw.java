da29740739cca8c1653d183934343e60f56b9c01
Gabriel Walford <gabriel@gabrielwalford.com>
2013-03-31 10:16:58 -0600

    Trying to overwrite gtw's merged file with final solution

--- projectEuler/euler4/euler4gtw.java
+++ projectEuler/euler4/euler4gtw.java
@@ -8,41 +8,22 @@ public static void main(String[] argv){
 	int x = 0;
 	int y = 0;
 
-<<<<<<< HEAD
 	for(int i = 1; i<=1000; i++){
 
 		for(int j = 1; j<=1000; j++){
-=======
-	for(int i = 1; i<10; i++){
-
-		for(int j = 1; j<10; j++){
->>>>>>> 686c1174e59d3cf0c0eaf47ea533bf6b1b62d966
 			x = i*j;
-			out.println(x);
+			// out.println(x);
 
 			String pal = "" + x;
 			String lap = new StringBuffer(pal).reverse().toString();
 
-<<<<<<< HEAD
 			if(pal.equals(lap)){
 				if(x>y){
 				y = x;
 				out.println(i +" ++ " + j + " == " + pal + " // " + lap);
 				
 				}
-=======
-			out.print(pal + " ==> " + lap);
-
-			if(pal.equals(lap)) {
-				out.println("YAY!"); 
-			} else {
-				out.println("No");
->>>>>>> 686c1174e59d3cf0c0eaf47ea533bf6b1b62d966
 			}
-
-			// if(pal.equals(lap)){
-			// 	out.println(i +" ++ " + j + " == " + pal + " // " + lap);
-			// }
 
 
 		}// END FOR 2
