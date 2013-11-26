9237709a39a6192e3ce030062decc4e4408a21fa
Gabriel Walford <gwalford@cpbgroup.com>
2013-11-25 23:51:53 -0700

    added palindrome solution for problem 5

--- javascript/euler4/js/functions.js
+++ javascript/euler4/js/functions.js
@@ -4,10 +4,22 @@
 
 /* trigger when page is ready */
 $(document).ready(function (){
-	
-	var equation1 = (Math.PI * Math.pow(100, 1));
+	var result = 0;
 
-	$('#result').html(equation1);
+	for (var i = 100; i < 1000; i++) {
+		for(var j = i; j<1000; j++){
+			
+			var x = i*j;
+			x.toString();
+			var y = x.toString().split("").reverse().join("");
+
+			if(x == y && x > result){
+				result = x;
+			}
+		}
+	};
+
+	$('#result').html(result);
 
 });
 
