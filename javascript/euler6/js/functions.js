c00673b715673f36662bafaa8ea524c9d8192774
Gabriel Walford <gwalford@cpbgroup.com>
2013-11-26 15:31:23 -0700

    setup problems 6-10 and began 6

---
+++ javascript/euler6/js/functions.js
@@ -0,0 +1,13 @@
+// remap jQuery to $
+(function($){
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+	var result = 0;
+	
+	$('#result').html(result);
+
+});
+
+})(window.jQuery);
