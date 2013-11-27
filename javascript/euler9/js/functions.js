d49b36e5f0bc4387aed8413a3515e88f6f97b8d7
Gabriel Walford <gwalford@cpbgroup.com>
2013-11-27 15:02:04 -0700

    added solution for problem 9

--- javascript/euler9/js/functions.js
+++ javascript/euler9/js/functions.js
@@ -4,8 +4,22 @@
 
 /* trigger when page is ready */
 $(document).ready(function (){
-	var result = 0;
+	var result = 0,
+		a = 3,
+		b = 4,
+		c = 0;
 	
+	while( a+b+c < 10000){
+		for(b = 2; b < 500; b++){
+			c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+			if(c%1 == 0 && (a+b+c) == 1000){
+				result = a*b*c;
+				break;
+			}
+		}
+		a++;
+	}
+
 	$('#result').html(result);
 
 });
