100ac7be75a495b9444cdfe0682729031c5498f7
Gabriel Walford <gwalford@cpbgroup.com>
2013-11-27 00:00:30 -0700

    added solutions to problem 7 and 8

--- javascript/euler7/js/functions.js
+++ javascript/euler7/js/functions.js
@@ -4,9 +4,29 @@
 
 /* trigger when page is ready */
 $(document).ready(function (){
-	var result = 0;
+	var result = 0,
+		num = 2;
+		count = 0;
+
+	while(count < 10001){
+		if( isPrime(num) ){
+			result = num;
+			count++;
+		}
+		num++;
+	}
+
 	
 	$('#result').html(result);
+
+	function isPrime (num) {
+		for(var j = 2; j < num; j++) {
+			if( num % j == 0 ){
+				return false;
+			}
+		}
+		return true;
+	}
 
 });
 
