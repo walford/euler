0239c7fb6e29e436a24a46438b821a19101909fa
bpwalford <bpwalford@gmail.com>
2014-11-27 14:20:51 -0700

    changed to use PointInTime class and increment instance attributes (not solved)

--- ruby/euler19.rb
+++ ruby/euler19.rb
@@ -11,26 +11,107 @@
 #
 #  How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-days   = %w[mon tue wed thr fri sat sun]
-months =  %w[jan feb mar apr may jun jul aug sep oct nov dec]
+require 'pry'
 
-leap      = false
-year      = 1900
-day       = days.first
-month     = montsh.first
-month_day = 1
+$mondays_on_first = 0
 
-mondays_on_first = 0
+class PointInTime
+
+  attr_accessor :days, :months, :leap, :year, :day, :month, :month_day
+
+  def initialize(days, months, leap, year, month_day)
+    self.days = days
+    self.months = months
+    self.leap = leap
+    self.year = year
+    self.month_day = month_day
+
+    self.day = days.first
+    self.month = months.first
+  end
 
 
-def incrementer
+  def incremente
 
-  if year > 2000
-    return
-  elsif month_day == 1 && day == 'mon'
-    mondays_on_first += 1
-  else
-    # incriment here
+    puts "#{year} : #{month} : #{day} : #{month_day}"
+
+    $mondays_on_first += 1 if (self.month_day == 1 && self.day == 'mon')
+
+    # increment day of week
+    check_day(self.day, self.days)
+
+    # check leap year
+    (self.year % 4 == 0) ? self.leap = true : self.leap = false
+
+    # increment february
+    if self.month == 'feb' && self.leap == true
+      if self.month_day >= 29
+        check_month(self.month, self.months)
+      else
+        self.month_day += 1
+      end
+    else
+      if self.month_day >= 28
+        check_month(self.month, self.months)
+      else
+        self.month_day += 1
+      end
+    end
+
+    # increment months other than february
+    if self.month == 'sep' || self.month == 'apr' || self.month == 'jun' || self.month == 'nov'
+      if self.month_day >= 30
+        check_month(self.month, self.months)
+      else
+        self.month_day += 1
+      end
+    elsif
+      if self.month_day >= 31
+        check_month(self.month, self.months)
+      else
+        self.month_day += 1
+      end
+    end
+
+    # increment year
+    self.year += 1 if (self.month == 'dec' && self.month_day == 31)
+
+  end
+
+  def check_month(month, months)
+    month == months.last ? month = months.first : month = months[months.index(month)+1]
+  end
+
+  def check_day(day, days)
+    day == days.last ? day = days.first : day = days[days.index(day)+1]
   end
 
 end
+
+time = PointInTime.new(
+  %w[mon tue wed thr fri sat sun],
+  %w[jan feb mar apr may jun jul aug sep oct nov dec],
+  false,
+  1900,
+  1
+)
+# binding.pry
+# while time.year < 2000
+#   time.incremente
+# end
+100.times do
+  time.incremente
+end
+
+puts $mondays_on_first
+
+
+
+
+
+
+
+
+
+
+# end
